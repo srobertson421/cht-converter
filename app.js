a7b70ca225f2853ea4daa1a7bec866ddd3b526b6
Sean Robertson <srobertson421@gmail.com>
2022-03-28 17:08:08 -0700

    fixed routing and initial page load

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-const renderEl = document.getElementById('render');
+//import Observable from 'https://sean-cdn.netlify.app/js/observable/@0.0.1/observable.min.js';
+
+const mainNavEl = document.getElementById('main-nav');
+const newCheatTemplate = document.getElementById('new-cheat-template');
+const pageViews = document.querySelectorAll('.page-view');
+const addCheatBtn = document.getElementById('add-cheat-btn');
 
 const validViews = ['home', 'converter', 'db'];
 
+let cheatCount = 0;
+
 function changeCurrentView(view) {
   if(validViews.includes(view)) {
-    const viewTemplate = document.getElementById(`${view}-template`);
-    renderEl.innerHTML = '';
-    renderEl.appendChild(viewTemplate.content.cloneNode(true));
+    pageViews.forEach(node => node.style.display = 'none');
+    document.getElementById(`${view}-template`).style.display = 'block';
   } else {
-    const viewTemplate = document.getElementById(`404-template`);
-    renderEl.innerHTML = '';
-    renderEl.appendChild(viewTemplate.content.cloneNode(true));
+    pageViews.forEach(node => node.style.display = 'none');
+    document.getElementById(`404-template`).style.display = 'block';
   }
 }
 
+function addNewCheatForm() {
+  const newGameForm = document.getElementById('new-game-form');
+  if(newGameForm) {
+    cheatCount++;
+    const cheatTemplateClone = newCheatTemplate.content.cloneNode(true);
+    cheatTemplateClone.querySelector('.cheat-title-label').setAttribute('for', `cheat-title-${cheatCount}`);
+    cheatTemplateClone.querySelector('.cheat-title-input').setAttribute('id', `cheat-title-${cheatCount}`);
+    cheatTemplateClone.querySelector('.cheat-code-label').setAttribute('for', `cheat-code-${cheatCount}`);
+    cheatTemplateClone.querySelector('.cheat-code-input').setAttribute('id', `cheat-code-${cheatCount}`);
+    newGameForm.firstElementChild.appendChild(cheatTemplateClone);
+  }
+}
+
+// routing
 window.addEventListener('hashchange', e => {
   changeCurrentView(location.hash.slice(1) || 'home');
 });
 
+addCheatBtn.addEventListener('click', () => {
+  addNewCheatForm();
+});
+
+// Untoggle nav checkbox when nav-link is clicked to close nav
+mainNavEl.addEventListener('click', e => {
+  if(e.target.tagName.toLowerCase() === 'a' && e.target.classList.contains('nav-link')) {
+    mainNavEl.querySelector('#menu-toggle').checked = false;
+  }
+});
+
+// Initial view render
 changeCurrentView(location.hash.slice(1) || 'home');
