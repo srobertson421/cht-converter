ecaabee08a479416b1fdd4cd4f4b4c9f7200e391
Sean Robertson <srobertson421@gmail.com>
2022-03-31 15:41:18 -0700

    test of function to edit json file

--- netlify/functions/getGame/getGame.js
+++ netlify/functions/getGame/getGame.js
@@ -1,13 +1,22 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const fs = require('fs');
+const path = require('path');
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const fileData = await new Promise((resolve, reject) => {
+      fs.readFile(path.join(__dirname, '..', 'test.json'), 'utf8', (err, data) => {
+        if(err) {
+          console.log(err);
+          reject(err);
+        }
+  
+        resolve(data);
+      });
+    });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Count is: ${JSON.parse(fileData).count}` }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
